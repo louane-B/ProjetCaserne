58bb99e2b0442d7f012a31b0db85f5cbf5b263c2
louane-B <louane31012004@gmail.com>
2026-04-29 12:51:01 -0400

    create function index of controller TypeInterventionController

---
+++ app/Http/Controllers/TypeInterventionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TypeInterventionController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Display the list of fire stations
+    |--------------------------------------------------------------------------
+    | If the user is on "/", load the home page (app.blade.php).
+    | Otherwise, load the main TypeIntervention page.
+    */
+    public function index()
+    {
+        // Load all Type intervention
+        $typeIntervention = TypeIntervention::get();
+        
+        // Otherwise, show the Type intervention list page
+        return view('typeIntervention', compact('typeIntervention'));
+    }
+}
