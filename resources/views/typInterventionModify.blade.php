339b2b0f62fd71455461d33b06ed8398724a7873
louane-B <louane31012004@gmail.com>
2026-05-01 15:30:34 -0400

    create view form edit typeIntervention

---
+++ resources/views/typInterventionModify.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+
+@section('content')
+
+{{-- Page title --}}
+<h1 class="text-center mb-4">Edit Type Intervention</h1>
+</br>
+
+{{-- Form to update an existing type intervention --}}
+<form action="{{ route('typeIntervention.update', $intervention->id) }}" method="POST" class="border p-4 bg-light rounded">
+    @csrf
+    @method('PUT') {{-- Laravel method override for PUT requests --}}
+
+    {{-- Type intervention number --}}
+    <div class="mb-3">
+        <label class="form-label">N° Type Intervention</label>
+        <input type="int" name="NoIntervention" class="form-control" value="{{ $intervention->NoIntervention }}" required>
+    </div>
+
+    {{-- Type Intervention description --}}
+    <div class="mb-3">
+        <label class="form-label">Description</label>
+        <input type="text" name="description" class="form-control" value="{{ $intervention->description }}" required>
+    </div>
+
+    {{-- Submit button --}}
+    <button type="submit" class="btn btn-primary w-100">Update Type Intervention</button>
+
+    {{-- Back button --}}
+    <a href="/TypeIntervention" class="btn btn-secondary w-100 mt-2">Back</a>
+</form>
+
+@endsection
