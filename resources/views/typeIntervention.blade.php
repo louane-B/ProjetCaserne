ae18d5ef56996752bc468f5d1d8f34c9e0d122d4
louane-B <louane31012004@gmail.com>
2026-05-01 10:22:18 -0400

    add message success in view typeIntervention

--- resources/views/typeIntervention.blade.php
+++ resources/views/typeIntervention.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
+{{-- Success message displayed after an action (add, update, delete, clear) --}}
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
 {{-- Main title of the page --}}
 <h1 class="mb-4">List of Type Interventions</h1>
 </br>
