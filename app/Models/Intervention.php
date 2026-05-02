b79070e71259f94685285f29f4f42618d0cc8022
louane-B <louane31012004@gmail.com>
2026-05-02 12:25:37 -0400

    create InterventionFactory

--- app/Models/Intervention.php
+++ app/Models/Intervention.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\TypeIntervention;
+use App\Models\FireStation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
---
+++ database/factories/InterventionFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Intervention;
+use App\Models\TypeIntervention;
+use App\Models\FireStation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Intervention>
+ */
+class InterventionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = Intervention::class;
+
+    public function definition(): array
+    {
+        return [
+            'DateTempsDebut' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'Adresse' => $this->faker->adress,
+            'Resume' => $this->faker->sentence(12),
+            'IdTypeIntervention' => TypeIntervention::inRandomOrder()->first()->id,
+            'IdCaserne' => FireStation::inRandomOrder()->first()->id,
+        ];
+    }
+}
