630ba3ab5d9102a37e108f5c40368f6c334f6570
louane-B <louane31012004@gmail.com>
2026-05-06 12:41:16 -0400

    modify migate grade

--- database/migrations/2026_05_06_162429_create_grades_table.php
+++ database/migrations/2026_05_06_162429_create_grades_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('grades', function (Blueprint $table) {
             $table->id();
+            $table->string('description');
+            $table->string('symbole')->nullable(); // symbole unicode or HTML
             $table->timestamps();
         });
     }
