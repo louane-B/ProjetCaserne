d7892e5195587fdd50b8a3cafc9ae4fe2521209a
louane-B <louane31012004@gmail.com>
2026-05-09 16:25:35 -0400

    create controller + function index firefighter

---
+++ app/Http/Controllers/FirefighterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirefighterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Display the list of firefighter
+    |---------------------------------------------------------------------------
+    | Otherwise, load the main FireStations page.
+    */
+    public function index()
+    {
+        // Load all fire stations with their associated state
+        $firefighter = Firefighter::with(['grade', 'station'])->get();
+        
+        // Otherwise, show the FireStations list page
+        return view('firefighter', compact('firefighters'));
+    }
+
+}
