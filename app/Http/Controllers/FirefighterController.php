baec5df8d26a7d216bc6d017049f46445d875a70
louane-B <louane31012004@gmail.com>
2026-05-09 19:47:27 -0400

    create function Add firefighter

--- app/Http/Controllers/FirefighterController.php
+++ app/Http/Controllers/FirefighterController.php
@@ -22,4 +22,33 @@ class FirefighterController extends Controller
         return view('firefighter', compact('firefighters'));
     }
 
+     /*
+    |--------------------------------------------------------------------------
+    | Add a new Firefighter
+    |--------------------------------------------------------------------------
+    | Validates the form, creates a new record, then redirects with a success message.
+    */
+    public function add(Request $request)
+    {
+        //validate form inputs
+        $request->validate([
+            'matricule' => 'required',
+            'grade_id' => 'required|exists:grade,id',
+            'nom' => 'required',
+            'prenom' => 'required',
+            'fire_station_id' => 'required|exists:station,id',
+        ]);
+
+        //Create the new Type Intervention
+        Firefighter::create([
+            'matricule'=>$request->matricule,
+            'grade_id'=>$request->grade_id,
+            'nom'=>$request->nom,
+            'prenom'=>$request->prenom,
+            'fire_station_id'=>$request->fire_station,
+        ]);
+
+        //Redirect with success message
+        return redirect('/firefighters')->with('success', 'firefighter successfully added');
+    }
 }
