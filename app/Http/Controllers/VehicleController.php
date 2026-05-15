e2c0df7fa955c8e11437e75e1f757ce680ced24a
louane-B <louane31012004@gmail.com>
2026-05-15 10:06:15 -0400

    create controller  Vehicle

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeVehicle;
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Display the list of Vehicle
+    |---------------------------------------------------------------------------
+    | Otherwise, load the main TypeVehicle page.
+    */
+    public function index()
+    {
+        // Load all type vehicles with their associated state
+        $Vehicles = Vehicle::all();
+        
+        // Otherwise, show the TypeVehicle list page
+        return view('Vehicle', compact('Vehicles'));
+    }
+}
