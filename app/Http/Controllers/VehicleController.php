b1b5a44efb7dbb03b4191adbc4f6f5fdedafcb5c
louane-B <louane31012004@gmail.com>
2026-05-15 10:48:41 -0400

    create function controller add vehicle

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -21,4 +21,39 @@ class VehicleController extends Controller
         // Otherwise, show the TypeVehicle list page
         return view('vehicle', compact('Vehicles'));
     }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Add a new vehicle
+    |--------------------------------------------------------------------------
+    | Validates the form, creates a new record, then redirects with a success message.
+    */
+    public function add(Request $request)
+    {
+        // Validate form inputs
+        $request->validate([
+            'NoIdentification' => 'required',
+            'Immatriculation' => 'required',
+            'AnneeMiseEnService' => 'required',
+            'Marque' => 'required',
+            'Modele' => 'required',
+            'type_vehicle_id' => 'required|exists:type_vehicles,id',
+            'fire_station_id' => 'required|exists:fire_stations,id',
+
+        ]);
+
+        // Create the new grade
+        Vehicle::create([
+            'NoIdentification' => $request->NoIdentification,
+            'Immatriculation' => $request->Immatriculation,
+            'AnneeMiseEnService' => $request->AnneeMiseEnService,
+            'Marque' => $request->Marque,
+            'Modele' => $request->Modele,
+            'type_vehicle_id' => $request->type_vehicle_id,
+            'fire_station_id' => $request->fire_station_id,
+        ]);
+
+        // Redirect with success message
+        return redirect('/vehicles')->with('success', 'Vehicle successfully added');
+    }
 }
